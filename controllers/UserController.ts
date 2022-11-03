7957c3a5d06550f4dad2ff978784b2da859a2f59
Gino <romangino@gmail.com>
2022-11-03 19:17:55 -0400

    fixed delete api

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -48,11 +48,11 @@ export default class UserController implements UserControllerI {
             // for testing. Not RESTful
             app.get("/api/users/create",
                 UserController.userController.createUser);
-            app.get("/api/users/id/:uid/delete",
+            app.delete("/api/users/id/:uid/delete",
                 UserController.userController.deleteUser);
-            app.get("/api/users/username/:username/delete",
+            app.delete("/api/users/username/:username/delete",
                 UserController.userController.deleteUsersByUsername);
-            app.get("/api/users/delete",
+            app.delete("/api/users/delete",
                 UserController.userController.deleteAllUsers);
         }
         return UserController.userController;
