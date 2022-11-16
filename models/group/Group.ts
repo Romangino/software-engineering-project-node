36d0b31e65d352d16e7573e16a67c6891ac71521
kylefairbairn <kyle.fairbairn@gmail.com>
2022-11-16 16:57:56 -0500

    added model for Group

---
+++ models/group/Group.ts
@@ -0,0 +1,23 @@
+/**
+ * @file Declare Group data type representing a group.
+ */
+
+import mongoose, {Collection} from "mongoose";
+
+/**
+ * @typedef Group Represents a group of user
+ * @property {Group} id group's id
+ * @property {Collection<String>} members A list of members to the group
+ * @property {Date} createdOn Date of when the group was created
+ * @property {Collection<String>} admin A list of members that are admins of the group
+ * @property {string} groupName group's name
+ * @property {string} description group's name
+ */
+export default interface Group {
+    _id?: mongoose.Schema.Types.ObjectId,
+    members: Collection<String>
+    createdOn?: Date
+    admin: Collection<String>
+    groupName: string
+    description: string
+};
