cb21e3d9b619880f800045d066b71c18e224d49a
adityaputtigampala <aditya.puttigampala@gmail.com>
2022-11-27 16:57:36 -0500

    updated field names in message model and mongoose schema

--- mongoose/messages/MessageSchema.ts
+++ mongoose/messages/MessageSchema.ts
@@ -5,9 +5,9 @@ import mongoose, {Schema} from "mongoose";
 import Message from "../../models/messages/Message"
 
 const MessageSchema = new mongoose.Schema<Message>({
-    message: {type: String, required: true},
-    to: {type: Schema.Types.ObjectId, ref: "GroupModel"},
-    from: {type: Schema.Types.ObjectId, ref: "UserModel"},
-    date: {type: Date, default: Date.now()}
+    content: {type: String, required: true},
+    group: {type: Schema.Types.ObjectId, ref: "GroupModel"},
+    sentBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+    sentOn: {type: Date, default: Date.now()}
 }, {collection: "messages"});
 export default MessageSchema
