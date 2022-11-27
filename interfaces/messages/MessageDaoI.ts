c58acae4edf37c46ec461edc693cacbd2ca07099
adityaputtigampala <aditya.puttigampala@gmail.com>
2022-11-27 14:41:36 -0500

    update messageControllerI and messageDaoI with group messaging functions

--- interfaces/messages/MessageDaoI.ts
+++ interfaces/messages/MessageDaoI.ts
@@ -6,11 +6,15 @@ import Message from "../../models/messages/Message";
 export default interface MessageDaoI {
     findAllMessages(): Promise<Message[]>;
 
-    userMessageUser(uid: string, ouid: string, message: Message): Promise<Message>;
+    userMessageGroup(uid: string, ouid: string, message: Message): Promise<Message>;
 
     findAllMessagesSentByUser(uid: string): Promise<Message[]>;
+
+    findAllMessagesInGroup(gid: string): Promise<Message[]>;
 
     findAllMessagesReceivedByUser(uid: string): Promise<Message[]>;
 
     userDeleteMessage(mid: string): Promise<any>;
+
+    userEditMessage(mid: string, message: Message): Promise<any>;
 }
